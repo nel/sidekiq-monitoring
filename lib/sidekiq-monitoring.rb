8c62e98ef1b3e491889fc31a159cf3c8258527a1
Vincent Boisard <vincent.boisard@dimelo.com>
2016-03-14 12:18:11 +0100

    better: added latency and execution time monitoring.

--- lib/sidekiq-monitoring.rb
+++ lib/sidekiq-monitoring.rb
@@ -3,20 +3,24 @@ require 'multi_json'
 
 class SidekiqMonitoring < Sinatra::Base
 
-  # Set yours down thresholds configuration
+  # Set your down thresholds configuration
   # {'default' => [ 1_000, 2_000 ], 'low' => [ 10_000, 20_000 ] }
   def self.thresholds=(thresholds)
     @@thresholds = thresholds
   end
   @@thresholds = {}
 
+  def self.latency_thresholds=(latency_thresholds)
+    @@latency_thresholds = latency_thresholds
+  end
+  @@latency_thresholds = {}
+
   get '/sidekiq_queues' do
     content_type :json
     MultiJson.dump SidekiqMonitoring::Global.new(@@thresholds)
   end
 
-  class Queue
-
+  module Monitorable
     ALERT_STATUS = {
       'OK' => 0,
       'WARNING' => 1,
@@ -24,11 +28,71 @@ class SidekiqMonitoring < Sinatra::Base
       'UNKNOWN' => 3
     }
 
+    attr_accessor :warning_threshold, :critical_threshold, :status
+
+    def <=>(other)
+      ALERT_STATUS[status] <=> ALERT_STATUS[other.status]
+    end
+
+  end
+
+  class Worker
+    include Monitorable
+
+    DEFAULT_THRESHOLD = [ 60, 120 ]
+
+    attr_accessor :process_id, :jid, :run_at, :queue, :worker_class
+
+    def initialize(process_id, jid, run_at, queue, worker_class, thresholds = nil)
+      @process_id = process_id
+      @jid = jid
+      @run_at = run_at
+      @queue = queue
+      @worker_class = worker_class
+      @warning_threshold, @critical_threshold = thresholds ? thresholds : DEFAULT_THRESHOLD
+    end
+
+    def as_json
+      {
+        'queue' => queue,
+        'jid' => jid,
+        'worker_class' => worker_class,
+        'elapsed_time' => elapsed_time,
+        'status' => status,
+        'process_id' => process_id
+      }
+    end
+
+    def elapsed_time
+      @elapsed_time ||= Time.now.to_i - run_at
+    end
+
+    def status
+      return 'CRITICAL' if elapsed_time >= critical_threshold
+      return 'WARNING' if elapsed_time >= warning_threshold
+      'OK'
+    end
+
+  end
+
+  class Queue
+    include Monitorable
+
     DEFAULT_THRESHOLD = [ 1_000, 2_000 ]
+    DEFAULT_LATENCY_THRESHOLD = [ 300, 900 ]
 
-    attr_accessor :name, :size, :warning_threshold, :critical_threshold, :status
+    attr_accessor :name, :size, :latency, :warning_latency_threshold, :critical_latency_threshold
 
-    def as_json(options = {})
+    def initialize(name, size, latency, thresholds = nil, latency_thresholds = nil)
+      @name = name
+      @size = size
+      @latency = latency
+      @warning_threshold, @critical_threshold = (thresholds ? thresholds : DEFAULT_THRESHOLD)
+      @warning_latency_threshold, @critical_latency_threshold = (latency_thresholds ? latency_thresholds : DEFAULT_LATENCY_THRESHOLD)
+      @status = monitoring_status
+    end
+
+    def as_json
       {
         'name' => name,
         'size' => size,
@@ -38,20 +102,9 @@ class SidekiqMonitoring < Sinatra::Base
       }
     end
 
-    def initialize(name, size, thresholds = nil)
-      @name = name
-      @size = size
-      @warning_threshold, @critical_threshold = (thresholds ? thresholds : DEFAULT_THRESHOLD)
-      @status = monitoring_status
-    end
-
-    def <=>(other)
-      ALERT_STATUS[status] <=> ALERT_STATUS[other.status]
-    end
-
     def monitoring_status
-      return 'CRITICAL' if size >= critical_threshold
-      return 'WARNING' if size >= warning_threshold
+      return 'CRITICAL' if size >= critical_threshold || latency >= critical_latency_threshold
+      return 'WARNING' if size >= warning_threshold || latency >= warning_latency_threshold
       'OK'
     end
 
@@ -59,31 +112,51 @@ class SidekiqMonitoring < Sinatra::Base
 
   class Global
 
-    attr_accessor :thresholds
+    attr_accessor :thresholds, :latency_thresholds
 
     def as_json(options = {})
       {
         'global_status' => global_status,
-        'queues' => queues.map(&:as_json)
+        'queues' => queues.map(&:as_json),
+        'workers' => workers.map(&:as_json)
       }
     end
 
     def global_status
-      @global_status ||= (queues.sort.last && queues.sort.last.status) || 'UNKNOWN'
+      queue_status = (queues.sort.last && queues.sort.last.status) || 'UNKNOWN'
+      worker_status = (workers.sort.last && workers.sort.last.status) || 'UNKNOWN'
+      status = if worker_status != 'UNKNOWN' && Monitorable::ALERT_STATUS[worker_status] > Monitorable::ALERT_STATUS[queue_status]
+        worker_status
+      else
+        queue_status
+      end
+      @global_status ||= status
     end
 
-    def initialize(thresholds = {})
+    def initialize(thresholds = {}, latency_thresholds = {})
       @thresholds = thresholds
+      @latency_thresholds = latency_thresholds
     end
 
     def queues
-      @queues ||= Sidekiq::Queue.all.collect{ |queue|
-        Queue.new(queue.name, queue.size, thresholds_from_queue(queue.name))
-      }
+      @queues ||= Sidekiq::Queue.all.map do |queue|
+        Queue.new(queue.name, queue.size, queue.latency, thresholds_from_queue(queue.name), latency_thresholds_from_queue(queue.name))
+      end
+    end
+
+    def workers
+      @workers ||= Sidekiq::Workers.new.map do |process_id, thread_id, work|
+        payload = work['payload']
+        Worker.new(process_id, payload['jid'], work['run_at'], work['queue'], payload['class'], latency_thresholds_from_queue(work['queue']))
+      end
     end
 
     def thresholds_from_queue(queue_name)
       (thresholds || {})[queue_name]
+    end
+
+    def latency_thresholds_from_queue(queue_name)
+      (latency_thresholds || {})[queue_name]
     end
 
   end
